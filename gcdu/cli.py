f02502fbd4a332309e8b871ef323042b5ce56b51
Kieras <akieras@ciandt.com>
2018-01-09 22:13:01 -0200

    Import command implementation.

--- gcdu/cli.py
+++ gcdu/cli.py
@@ -3,6 +3,7 @@ import click
 
 from __version__ import __version__ as version
 from commands.export import export
+from commands.import_cmd import import_cmd
 
 
 @click.group()
@@ -12,3 +13,4 @@ def main():
     pass
 
 main.add_command(export)
+main.add_command(import_cmd)
--- gcdu/commands/export.py
+++ gcdu/commands/export.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 """The export command."""
-import io
-import os
 import json
 import click
-import googleapiclient.discovery
 
-from .utils import show_progressbar_item, replace_with_placeholders
+from .utils import get_datastore_api, get_kinds_list, show_progressbar_item, partition_replace, save
 
 
 @click.command()
@@ -36,7 +33,7 @@ from .utils import show_progressbar_item, replace_with_placeholders
               required=True)
 def export(project, namespace, data_dir, project_placeholder, namespace_placeholder, kinds):
     """Export data from database."""
-    kinds_list = kinds.split(',')
+    kinds_list = get_kinds_list(kinds)
     click.echo("Executing export. Project={}, Namespace={}, Kinds={}.".format(project, namespace, kinds_list))
     with click.progressbar(kinds_list, label='Exporting', show_eta=True,
                            item_show_func=show_progressbar_item) as bar:
@@ -45,7 +42,7 @@ def export(project, namespace, data_dir, project_placeholder, namespace_placehol
 
 
 def execute_export(project, namespace, data_dir, project_placeholder, namespace_placeholder, kind):
-    datastore = googleapiclient.discovery.build('datastore', 'v1')
+    datastore = get_datastore_api()
 
     request_body = {
         'partitionId': {
@@ -67,7 +64,7 @@ def execute_export(project, namespace, data_dir, project_placeholder, namespace_
 
     entities = extract_entities(response)
     entities_json = json.dumps(entities)
-    entities_replaced_json = replace_with_placeholders(entities_json, project, project_placeholder, namespace,
+    entities_replaced_json = partition_replace(entities_json, project, project_placeholder, namespace,
                                                        namespace_placeholder)
     entities_replaced = json.loads(entities_replaced_json)
 
@@ -80,12 +77,3 @@ def extract_entities(response):
         entity = entityResult.get('entity')
         entities.append(entity)
     return entities
-
-
-def save(entities, kind, data_dir):
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-
-    with io.open('{}/{}.json'.format(data_dir, kind), 'w', encoding='utf-8') as export_file:
-        export_file.write(
-            json.dumps(entities, ensure_ascii=False, sort_keys=True, indent=2, separators=(',', ': ')))
